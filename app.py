502cdd6f3851ca10c1051ee9607372612a9d2a0e
Enrique <8863865@estudiantes.edumelilla.es>
2026-04-08 01:16:12 +0200

    Paso 9: Cambio de texto en la web

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Servidor desplegado correctamente en Kubernetes!"
+    return "Enrique Julian ha desplegado el servidor"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
